d2363b5286bd996cc8aa1c24d4e05951176e8e75
Santiago-vdk <Santiago.vdk@gmail.com>
2014-09-09 00:54:14 -0600

    Cambios main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,11 @@
 #include <QApplication>
 #include <gui.h>
-#include <QDebug>
-#include "Facade.h"
-
-
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //gui w;
-    //.show();
-    Facade *facade = new Facade("rafa");
-    (*facade).crearObstaculo("Dinamico",5,5);
-    (*facade).crearObstaculo("Jefe",10,10);
-    (*facade).crearObstaculo("Estatico",5,5);
-    (*facade).crearObstaculo("Teledirigido",10,10);
-    (*facade).crearObstaculo("Rastrero",5,5);
-    (*facade).crearObstaculo("Volumen",10,10);
-    qDebug()<<(*facade).getCantObstaculos();
-    (*facade).borrarObstaculoEnPos(2);
-    qDebug()<<(*facade).getCantObstaculos();
-    qDebug()<<(*facade).getPosXObstaculoEnPos(2);
-    delete facade;
-
+    gui w;
+    w.show();
     return a.exec();
 }
 
