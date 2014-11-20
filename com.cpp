58bd09da760f6fb74b88ba932ce037c6dc5c4fe2
David Badilla <davbs94@gmail.com>
2014-11-20 00:40:06 -0600

    Comunicacion serial

---
+++ com.cpp
@@ -0,0 +1,42 @@
+#include "com.h"
+
+com::com()
+{
+    Port = new QSerialPort();
+    Port->setPortName("COM4");
+}
+
+void com::habilitar(){
+    if(Port->open(QIODevice::ReadWrite))
+        {
+
+            Port->setBaudRate(QSerialPort::Baud9600);
+            Port->setDataBits(QSerialPort::Data8);
+            Port->setParity(QSerialPort::NoParity);
+            Port->setStopBits(QSerialPort::OneStop);
+            Port->setFlowControl(QSerialPort::NoFlowControl);
+
+            qDebug()<<"Puerto habilitado";
+        }
+    else{
+       qDebug()<<"Error al habilitar el puerto";
+    }
+}
+
+bool com::cerrar(){
+    Port->close();
+}
+
+
+
+QString com::leer()
+{
+    qDebug()<<"Antes de leer";
+    while (1) {
+        int numBytes= Port->bytesAvailable();
+        Port->waitForReadyRead(50);
+        if (numBytes>0){
+            qDebug()<<Port->readAll();
+        }
+    }
+}
